fc14e2ca272cc00910723ed7ecc08f62d2dc2e59
pulidongz <paogravoso@gmail.com>
2022-02-04 15:35:07 +0800

    Fix: Added create sql file if dne. Added complete db list.

--- PyImportToMySQL.py
+++ PyImportToMySQL.py
@@ -3,6 +3,7 @@ import time
 import traceback
 import os
 from dotenv import load_dotenv
+from pathlib import Path
 load_dotenv()
 
 """
@@ -10,8 +11,7 @@ SCRIPT FOR MYSQL DATABASE BACKUP AND IMPORT
     - Don't forget to set the environment variables in the .env file
 """
 
-# DATABASE = ["analysis_db", "comms_db", "commons_db", "ewi_db"]
-DATABASE = ["analysis_db"]
+DATABASE = ["analysis_db", "comms_db", "commons_db", "ewi_db"]
 
 REMOTE_USERNAME = os.getenv("REMOTE_USERNAME")
 REMOTE_PASSWORD = os.getenv("REMOTE_PASSWORD")
@@ -26,18 +26,20 @@ def BackupAndImport():
         time_start = time.perf_counter()
 
         print(f"\nCreating dump file for {db}")
-        command = ['mysqldump', f'-u{REMOTE_USERNAME}', f'-p{REMOTE_PASSWORD}', f'-h{REMOTE_HOST}', '--column-statistics=0', f'{db}']
+        command = ['mysqldump', f'-u{REMOTE_USERNAME}', f'-p{REMOTE_PASSWORD}', f'-h{REMOTE_HOST}', '--column-statistics=0', '--verbose', f'{db}']
         with open('ewi_db.sql', 'w') as outfile:
             subprocess.run(command, stdout = outfile)
 
         print(f"\nImporting {db} to MySQL")
+        file = Path(f'{db}.sql')
+        file.touch(exist_ok=True)
         with open(f'{db}.sql', 'r') as f: 
             command = ['mysql', f'-u{LOCAL_USERNAME}', f'-p{LOCAL_PASSWORD}', f'{db}']
             proc = subprocess.Popen(command, stdin = f)
             stdout, stderr = proc.communicate()
         
         time_delta = time.perf_counter() - time_start
-        print(f"Done! Time elapsed: {time_delta} seconds")
+        print(f"Done! Total time elapsed: {time_delta} seconds")
         print("-"*64)
     
     print(f"\nDATABASE BACKUP COMPLETE. \nOperation took {time.perf_counter() - time_start} seconds")
